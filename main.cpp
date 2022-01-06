967a665ff40395e28f04e1568f6df1816d4c5bd4
GrowLK <34890135+GrowLK@users.noreply.github.com>
2022-01-06 18:58:35 +0800

    Update main.cpp
    linux下带颜色显示二维码

--- main.cpp
+++ main.cpp
@@ -3,16 +3,20 @@
 # include <cstdio>
 
 int main(int argc, char *argv[]) {
-    string str = "abcdefghijklmnopqrstuvwxyzabcdefghijkl";
+    if (argc != 2 ) {
+        printf("usage: ./main <message>");
+        return 0;
+    }
+    string str(argv[1]);
     QRCode *qrcode = new QRCode(str, QRCode::QUITE);
     vector<vector<QRCode::Color>> pattern = qrcode->getPattern();
     for (int i=0; i<pattern.size(); ++i) {
         for (int j=0; j<pattern[i].size(); ++j) {
-            if (pattern[i][j]==QRCode::DARK) printf("++");
-            else printf("--");
+            if (pattern[i][j]==QRCode::DARK) printf("\033[44m  \033[0m");
+            else printf("\033[47m  \033[0m");
         }
+        printf("\n");
     }
-    printf("\n");
     delete qrcode;
     return 0;
 }
